fcc9cd7ac7018ee70ad03fc834b9a5a76257f0d4
Justin C <jchoi03@bu.edu>
2025-02-28 18:10:29 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember()
+{
+    var member = {
+        name: "JC",
+        age: 30,
+        skills: ["Javascript", "HTML", "CSS"],
+        address: {
+            city: "New York",
+            country: "USA"
+        }
+    };
+
+    return member;
+}
